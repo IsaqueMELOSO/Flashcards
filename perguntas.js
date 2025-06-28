fb28bbf239fd7ccb8a95871d585b278544900a97
IsaqueMELOSO <isaque.montoski@escola.pr.gov.br>
2025-06-28 11:24:29 -0300

    ..

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+CriaCartao (
+    'Futebol',
+    'Qual é o jogador com mais bolas de ouro?',
+    'O jogador com mais bolas de ouro é o Lionel Messi'
+)
+
+CriaCartao (
+    'Futebol',
+    'Qual foi a seleção que ganhou a Copa do Mundo de 2022?',
+    'A seleção que ganhou a copa do mumdo de 2022 foi a Argentina'
+)
+
+CriaCartao (
+    'Futebol',
+    'Quem é considerado o "Pai do Futebol" no Brasil?',
+    'O "Pai do Futebol" considerdo no Brasil é o Charles Miller'
+)
+
+CriaCartao (
+    Futebol,
+    pergunta,
+    resposta
+)
+
+CriaCartao (
+    Futebol,
+    pergunta,
+    resposta
+)
